0182e93c8ffd6ac5b07f988e5e7a952ed03b47fa
kingstar1227 <tedd8785@gmail.com>
2017-12-30 19:14:18 +0300

    :hammer Refactor page hero.

---
+++ app/components/PageHero/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import PageHero from '../index';
+
+describe('<PageHero />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
