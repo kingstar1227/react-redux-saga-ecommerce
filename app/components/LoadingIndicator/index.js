34264578c823390d79a0a71ca27b236d1d5fe2ba
kingstar1227 <tedd8785@gmail.com>
2018-01-02 16:53:16 +0300

    Add more loaders.

--- app/components/LoadingIndicator/index.js
+++ app/components/LoadingIndicator/index.js
@@ -41,3 +41,4 @@ export const BubblingLoader = () => (
     </div>
   </FullpageIndicator>
 );
+
--- app/containers/CategoryPage/ProductCard.js
+++ app/containers/CategoryPage/ProductCard.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+
 import ProductButton from './ProductButton';
 
 const photoOne =
@@ -15,103 +16,127 @@ const products = [
     id: 1,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoOne,
+    image: photoOne
   },
   {
     id: 2,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoThree,
+    image: photoThree
   },
   {
     id: 6,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoThree,
+    image: photoThree
   },
   {
     id: 4,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoOne,
+    image: photoOne
   },
   {
     id: 7,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoOne,
+    image: photoOne
   },
   {
     id: 5,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoTwo,
+    image: photoTwo
   },
   {
     id: 8,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoTwo,
+    image: photoTwo
   },
   {
     id: 9,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoThree,
+    image: photoThree
   },
   {
     id: 3,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoTwo,
+    image: photoTwo
   },
   {
     id: 10,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoOne,
+    image: photoOne
   },
   {
     id: 11,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoTwo,
+    image: photoTwo
   },
   {
     id: 12,
     title: 'United Colors of Benetton',
     price: 1200,
-    image: photoThree,
-  },
+    image: photoThree
+  }
 ];
 
-export class ProductCard extends PureComponent {
+export class Product extends PureComponent {
   render() {
+    const { product } = this.props;
+    return (
+      <li>
+        <div className="category-product-wrap">
+          <div
+            className="category-product-image"
+            style={{
+              backgroundImage: `url(${product.image})`
+            }}
+          >
+            <ProductButton />
+          </div>
+          <div className="category-product-desc">
+            <span>{product.title}</span>
+            <span>{`KES ${product.price}`}</span>
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export class ProductLoader extends PureComponent {
+  render() {
+    const { product } = this.props;
+    return (
+      <li>
+        <div className="category-product-wrap">
+          <div className="category-product-image">
+            <div className="animated-background" />
+          </div>
+          <div className="category-product-desc" />
+        </div>
+      </li>
+    );
+  }
+}
+
+export default class ProductCard extends React.Component {
+  render() {
+    const isLoading = true;
     return (
       <div>
         <ul className="category-product">
           {products.map((product, index) => (
-            <li key={product.id}>
-              <div className="category-product-wrap">
-                <div
-                  className="category-product-image"
-                  style={{
-                    backgroundImage: `url(${product.image})`,
-                  }}
-                >
-                  <ProductButton />
-                </div>
-                <div className="category-product-desc">
-                  <span>{product.title}</span>
-                  <span>{`KES ${product.price}`}</span>
-                </div>
-              </div>
-            </li>
+            <Product product={product} key={product.id} />
           ))}
         </ul>
       </div>
     );
   }
 }
-
-export default ProductCard;
