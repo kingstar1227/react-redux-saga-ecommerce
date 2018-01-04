cda639ebb0ae8bef00b309bd8eb544755e96853c
kingstar1227 <tedd8785@gmail.com>
2018-01-04 09:40:26 +0300

    Added loaders.

--- app/containers/CartPage/Loadable.js
+++ app/containers/CartPage/Loadable.js
@@ -6,7 +6,10 @@
 
 import Loadable from 'react-loadable';
 
+import LoadingIndicator from 'components/LoadingIndicator';
+
 export default Loadable({
   loader: () => import('./index'),
-  loading: () => null,
+  loading: LoadingIndicator,
 });
+
